050d232bcbdba0a44103be9c9de894c192cbe2de
balarksinghal21 <balark.singhal21@gmail.com>
2020-02-07 23:32:58 +0530

    commented the code for easy evaluation

--- tictactoe-python.py
+++ tictactoe-python.py
@@ -1,10 +1,10 @@
-pos = [" " for i in range(10)]
-pos[5] = "O"
+pos = [" " for i in range(10)] #creating a list for board with a empty 0 value
+pos[5] = "O"#as requested in the game
 
-def inslet(lett,posi):
+def inslet(lett,posi):#function to insert letter on the board
     pos[posi] = lett
 
-def spacefree(posi):
+def spacefree(posi):#function to check if the position selected is free or not
     return pos[posi] == " "
 
 def print_board():#to display values on board
@@ -14,9 +14,9 @@ def print_board():#to display values on board
     print("-----|-----|-----")
     print(" ",pos[7]," | ",pos[8]," | ",pos[9])
 
-def comp_select():
-    possiblemoves = [x for x,letter in enumerate(pos) if letter ==" " and x != 0]
-    move = 0
+def comp_select():#coded for computer to select values for its moves
+    possiblemoves = [x for x,letter in enumerate(pos) if letter ==" " and x != 0]#to check spaces free on board
+    move = 0#condition on which loop will end
 
     for let in ["X"]:
         for i in possiblemoves:
@@ -56,8 +56,8 @@ def comp_select():
         move = selectrandom(possiblemoves)
         return move
 
-def selectrandom(lis):
-    import random
+def selectrandom(lis):#function to select a randome position value from the available moves for computer
+    import random #randomiser or random value selecter library
     ln =len(lis)
     if(len(lis)>0):
         r = random.randrange(0,ln)
@@ -65,10 +65,10 @@ def selectrandom(lis):
     else:
         return 0
 
-def is_win(bo,le):
+def is_win(bo,le): # returns true or false on basis on conditions below
     return (bo[1] == le and bo[2] == le and bo[3] == le) or (bo[1] == le and bo[4] == le and bo[7] == le) or (bo[3] == le and bo[6] == le and bo[9] == le) or (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or (bo[2] == le and bo[5] == le and bo[8] == le) or (bo[1] == le and bo[5] == le and bo[9] == le) or (bo[3] == le and bo[5] == le and bo[7] == le)
-    
-def usr_ent():
+      # Forced to code in a single line due to error
+def usr_ent():#Function to take user entry and insert letter on basis of conditions
     run =True
     while run:
         ent = input("Please enter a Position to insert a 'X'(1-9): ")
@@ -85,13 +85,13 @@ def usr_ent():
         except:
             print("Please Type A Number!")
 
-def isBoardfull(board):
+def isBoardfull(board): #if board is full without winning,tie game - this function checks if board is full
     if pos.count(" ") > 1 :
         return False
     else:
         return True
 
-def main():
+def main(): #this is basically start of the program-i.e. - First Run
     print("Welcome To Tic-Tac-Toe  Game!!!\n")
     print("Rules Are As Follows: \n1). You Are Playing As a 'X'\n2). There is Already a 'O' Present in the middle\n3). First One to get a 3 in row WINS.\n\n")
     print_board()
@@ -100,7 +100,7 @@ def main():
             usr_ent()
             print_board()
         else:
-            print("Sorry!,O\'s won This Time")
+            print("Sorry!,O\'s won This Time") #Only used if u intentionally want computer to win
             break
         if not(is_win(pos,"X")):
             move = comp_select()
@@ -116,4 +116,4 @@ def main():
     if isBoardfull(pos):
         print("Tie Game")
     
-main()
+main() #Call to the main function & program executes - remove it and see what happens :)
